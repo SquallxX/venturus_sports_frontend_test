b2285d0588434360b54992debf542cfd60773ed8
Felipe de Alcântara Monteiro <36462206+SquallxX@users.noreply.github.com>
2019-07-26 11:52:11 -0300

    Added build folder for this application

---
+++ build/precache-manifest.e83ce1a2f81475eb6eb815b431061cd5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c572be51a84718f498e37f1953d494a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d59dd79eee654c9e36eb",
+    "url": "/static/css/2.400f043a.chunk.css"
+  },
+  {
+    "revision": "fb03eb071502f35c9356",
+    "url": "/static/css/main.4b330a82.chunk.css"
+  },
+  {
+    "revision": "d59dd79eee654c9e36eb",
+    "url": "/static/js/2.12da78ec.chunk.js"
+  },
+  {
+    "revision": "fb03eb071502f35c9356",
+    "url": "/static/js/main.90e40494.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
